b163d2600b17bf6dfa6d9be4fa29e0b405ea6adf
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-09-09 18:02:07 +0530

    The method creates a thread pool of the fixed size s

---
+++ src/WorkerThread.java
@@ -0,0 +1,17 @@
+
+
+
+class WorkerThread implements Runnable {
+    private final String message;
+    public WorkerThread(String s){
+        this.message=s;
+    }
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+" (Start) message = "+message);
+        processmessage();
+        System.out.println(Thread.currentThread().getName()+" (End)");
+    }
+    private void processmessage() {
+        try {  Thread.sleep(6000);  } catch (InterruptedException e) { e.printStackTrace(); }
+    }
+}
