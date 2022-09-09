b149080161f5bfb856a67ba558a9f42d213d5e37
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-09-09 11:48:59 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+/**
+ **** Thread:-
+     ---> Thread is a light weight sub process
+     ---> A thread is a process of executing multiple threads simultaneously is called multithreading
+Creating threads in Java :-
+     -->To create thread in java use the following approaches
+        1.By Extending the thread class
+        2.By Implementing the Runnable interface
+ */
+
+// Java program to create a thread by extending the thread class
+
+class Test extends Thread{
+    public void run(){
+
+        for(int i=1;i<=5;i++){
+            System.out.println(i);
+        }
+
     }
 }
+
+class Main{
+    public static void main(String[] args) {
+
+
+            Test m =new  Test();
+            m.start();
+
+
+    }
+}
+
